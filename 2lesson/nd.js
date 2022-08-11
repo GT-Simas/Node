427a7e082c20d6f694fc0c3410accbf35890cea5
GT-Simas <Simasproo@gmail.com>
2022-08-11 13:13:48 +0300

    time machine

---
+++ 2lesson/nd.js
@@ -0,0 +1,6 @@
+app.use(cors())
+
+const cars = [
+    { name: "BWM", model: "bimbalas" },
+    { name: "Wolksvagen", model: "Tiguan" },
+    ];
---
+++ 7lesson/index.js
@@ -0,0 +1,42 @@
+
+
+const express = require("express");
+const cors = require("cors");
+const { MongoClient } = require("mongodb");
+
+require("dotenv").config();
+
+const app = express();
+app.use(express.json());
+app.use(cors());
+
+const port = process.env.PORT || 8080;
+const uri = `mongodb+srv://simas:${process.env.PASSWORD}@cluster0.6u6czto.mongodb.net/test`;
+
+
+const client = new MongoClient(uri);
+
+app.get("/", async (req, res) => {
+  try {
+    const con = await client.connect(); // atidarom prisijungima
+    const data = await con.db("demo1").collection("cars").find().toArray(); // pasiimam duomenis
+    await con.close(); // uzdarom prisijungima
+    return res.send(data);
+  } catch (error) {
+    res.status(500).send({ error });
+  }
+});
+
+app.post("/", async (req, res) => {
+  try {
+    const con = await client.connect();
+    const dbRes = await con.db("demo1").collection("cars").insertOne(req.body);
+    await con.close();
+    return res.send(dbRes);
+  } catch (err) {
+    res.status(500).send({ err });
+  }
+});
+
+
+app.listen(port, () => console.log(`Server is running on port ${port}`));
